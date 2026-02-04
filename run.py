35caa61e756234d33ba495abc319e30f5653c61e
VitusPuttmann <vitus.puttmann@outlook.com>
2026-02-04 12:00:07 +0100

    feat: add cross-validation

--- run.py
+++ run.py
@@ -9,25 +9,36 @@ from src.mappings import (
     FEATURE_NAME_MAPPING,
     FEATURE_CAT_MAPPING
 )
+from src.cross_validation import CVConfig, iter_cv_folds
 
 
-RAW_TEST_DATA="test.csv"
 RAW_TRAIN_DATA="train.csv"
+RAW_TEST_DATA="test.csv"
+
+CFG=CVConfig(
+    n_splits=5,
+    shuffle=True,
+    random_state=483927,
+    stratify=True
+)
 
 
 if __name__ == "__main__":
     # Load raw data files
-    test_data_raw = load_raw_data(DIR_RAW_DATA, RAW_TEST_DATA)
     train_data_raw = load_raw_data(DIR_RAW_DATA, RAW_TRAIN_DATA)
+    test_data_raw = load_raw_data(DIR_RAW_DATA, RAW_TEST_DATA)
 
     # Rename features
-    test_data_renamed = rename_features(test_data_raw, FEATURE_NAME_MAPPING)
     train_data_renamed = rename_features(train_data_raw, FEATURE_NAME_MAPPING)
+    test_data_renamed = rename_features(test_data_raw, FEATURE_NAME_MAPPING)
 
     # Convert features
-    test_data_converted = test_data_renamed.copy()
     train_data_converted= train_data_renamed.copy()
+    test_data_converted = test_data_renamed.copy()
 
     for var_name, var_map in FEATURE_CAT_MAPPING.items():
-        test_data_converted = numeric_to_string(test_data_converted, var_name, var_map)
         train_data_converted = numeric_to_string(train_data_converted, var_name, var_map)
+        test_data_converted = numeric_to_string(test_data_converted, var_name, var_map)
+
+    # Create CV iterator
+    cv_iterator = iter_cv_folds(train_data_converted, "heart_disease", CFG)
