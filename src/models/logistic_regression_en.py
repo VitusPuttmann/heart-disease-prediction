3d9e66f9031213477e87edb0673a6f910c642b39
VitusPuttmann <vitus.puttmann@outlook.com>
2026-02-11 21:38:59 +0100

    feat: add elastic net logistic regression

---
+++ src/models/logistic_regression_en.py
@@ -0,0 +1,70 @@
+"""
+Functions for the training, evaluation and prediction for a elastic net
+logistic regression model.
+"""
+
+import pandas as pd
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+
+LREN_PARAMS = {
+    "solver" : "saga",
+    "max_iter": 500,
+    "C": 1.0,
+    "tol": 1e-3,
+    "l1_ratio": 0.5,
+    "class_weight": None,
+    "random_state": 907364
+}
+
+
+def fit_logistic_regression_en(
+    train_X: pd.DataFrame,
+    train_y: pd.Series,
+    params: dict,
+) -> LogisticRegression:
+    """
+    Fit a elastic net logistic regression model.
+    """
+
+    model = LogisticRegression(**params)
+
+    model.fit(train_X, train_y)
+
+    return model
+
+
+def evaluate_logistic_regression_en(
+    model: LogisticRegression,
+    val_X: pd.DataFrame,
+    val_y: pd.Series,
+) -> pd.DataFrame:
+    """
+    Evaluate a previously trained elastic net logistic regression model.
+    """
+
+    y_pred = model.predict(val_X)
+    y_proba = model.predict_proba(val_X)[:, 1]
+
+    acc = accuracy_score(val_y, y_pred)
+
+    return pd.DataFrame(
+        [[acc]],
+        columns=["accuracy"],
+    )
+
+
+def predict_logistic_regression_en(
+    model: LogisticRegression,
+    test_X: pd.DataFrame,
+) -> pd.Series:
+    """
+    Predict values for a test dataset based on a previously trained elastic
+    net logistic regression model.
+    """
+
+    proba = model.predict_proba(test_X)[:, 1]
+    
+    return pd.Series(proba)
